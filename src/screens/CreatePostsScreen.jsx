d24acf5baaf7d8d3ce805151e4c26c53c3dc990f
Anton K <akarpenko83@gmail.com>
2023-07-12 12:40:15 +0300

    add post screen ready

--- src/screens/CreatePostsScreen.jsx
+++ src/screens/CreatePostsScreen.jsx
@@ -1,10 +1,54 @@
-import { Text } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default CreatePostsScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>CreatePostsScreen</Text>
+      <View style={styles.photo}>
+        <Image
+          style={styles.cameraIcon}
+          source={require('../../assets/cameraIcon.png')}
+        />
+      </View>
+      <Text style={styles.text}>Завантажте фото</Text>
+      <KeyboardAvoidingView
+        style={styles.description}
+        behavior={
+          Platform.OS == 'ios' ? 'padding' : 'height'
+        }
+      >
+        <TextInput
+          label="Назва..."
+          value={null}
+          onChangeText={null}
+          style={styles.input}
+        />
+        <TextInput
+          label="Місцевість..."
+          value={null}
+          onChangeText={null}
+          left={
+            <TextInput.Icon icon="map-marker-plus-outline" />
+          }
+          style={styles.input}
+        />
+      </KeyboardAvoidingView>
+      <Button
+        disabled={true}
+        mode="contained"
+        buttonColor="darkorange"
+        onPress={() => null}
+        style={styles.publishBtn}
+      >
+        Опублікувати
+      </Button>
     </SafeAreaView>
   );
 };
@@ -12,5 +56,35 @@ export default CreatePostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    paddingHorizontal: 16,
+    justifyContent: 'flex-start',
+  },
+  photo: {
+    marginBottom: 8,
+    position: 'relative',
+    width: '100%',
+    height: 240,
+    backgroundColor: '#E8E8E8',
+    borderRadius: 6,
+  },
+  cameraIcon: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: [{ translateX: -30 }, { translateY: -30 }],
+    height: 60,
+    width: 60,
+  },
+  text: {
+    marginBottom: 22,
+    color: '#BDBDBD',
+    fontSize: 16,
+  },
+  input: {
+    marginBottom: 8,
+  },
+  description: {
+    marginBottom: 32,
   },
 });
